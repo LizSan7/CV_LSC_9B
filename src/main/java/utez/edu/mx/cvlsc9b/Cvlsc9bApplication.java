273fe629b091c120b1f1448abcdd4fccc186c176
LizSan7 <20223tn031@utez.edu.mx>
2025-05-27 15:50:49 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvlsc9b/Cvlsc9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvlsc9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvlsc9bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cvlsc9bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/mx/cvlsc9b/Cvlsc9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvlsc9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvlsc9bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
